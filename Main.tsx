925da9efe33a28b9034f64750cc1425dd82bcff4
Yodaman07 <ayaan.irshad@gmail.com>
2024-05-28 12:55:38 -0700

    added tabs with the react-native-tab-view library and guide on the website. It didn't turn out the way I expected, so I will be changing it to use the react-navigation libraries

--- Main.tsx
+++ Main.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react';
+import { TabView, SceneMap } from 'react-native-tab-view';
 import {
     StyleSheet,
     Text,
@@ -6,25 +6,37 @@ import {
     TextInput,
     Button
 } from 'react-native';
+import { useState } from 'react';
 
 
 function Main(): React.JSX.Element {
-    return (
+    const scene_1 = () => (<View style={styles.container}>
+        <Text style={styles.title} > Welcome to a testing game! </Text>
+        <TextInput style={styles.input} placeholder='Enter Username Here'></TextInput>
+        <Button title='Submit' />
+    </View >)
 
-        <View style={styles.container}>
-            <Text style={styles.title} > Welcome to a testing game! </Text>
-            <TextInput style={styles.input} placeholder='Enter Username Here'></TextInput>
-            <Button title='Submit' />
-        </View >
+    const scene_2 = () => (<View></View>)
+
+    const map = SceneMap({
+        "game": scene_1,
+        "settings": scene_2
+    });
+
+    const [index, setIndex] = useState(0)
+    const [routes] = useState(
+        [{ key: "game", title: "Game" }, { key: "settings", title: "Settings" }]
+    )
+
+    return (
+        <TabView //https://github.com/react-navigation/react-navigation/issues/11999
+            style={{ paddingTop: 40 }}
+            navigationState={{ index, routes }}
+            onIndexChange={setIndex}
+            renderScene={map}
+        />
     );
 }
-
-
-// function SubmitBtn(): React.JSX.Element {
-//     return (    
-//     )
-// }
-
 
 
 const styles = StyleSheet.create({
